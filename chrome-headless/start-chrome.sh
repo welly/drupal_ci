b309690307736f9056f3fa99e8f4f588c1403c4c
Alastair Moore <alastair.moore@tpximpact.com>
2024-04-15 18:46:19 +0100

    cleanup

--- chrome-headless/start-chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep -x "chrome" > /dev/null
-then
-  echo "Chrome already running!"
-else
-  echo "Running with: ${CHROME_OPTS}"
-  /usr/bin/google-chrome ${CHROME_OPTS}
-fi
